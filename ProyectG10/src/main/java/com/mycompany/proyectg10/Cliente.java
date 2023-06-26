bbfb0cce73b20d59b66a74cb123feb24a8cc0faf
kiretigr <kiretigr@espol.edu.ec>
2023-06-25 21:09:14 -0500

    Atributos, constructor, setters y getters en Cliente.java

--- ProyectG10/src/main/java/com/mycompany/proyectg10/Cliente.java
+++ ProyectG10/src/main/java/com/mycompany/proyectg10/Cliente.java
@@ -8,6 +8,35 @@ package com.mycompany.proyectg10;
  *
  * @author cmontes
  */
-public class Cliente {
+public class Cliente extends Usuario {
+    private int tarjetaCredito;
+    private int puntosLicencia;
+    private TipoCliente tipoC;
+    
+    public Cliente(int numCedula, String nombre, String apellidos, int edad, TipoUsuario perfil, String correo, String contrasenia, int tarjetaCredito, int puntosLicencia, TipoCliente tipoC){
+        super(numCedula,nombre,apellidos,edad,perfil,correo,contrasenia);
+        this.tarjetaCredito = tarjetaCredito;
+        this.puntosLicencia = puntosLicencia;
+        this.tipoC = tipoC;
+    }
+    
+    public int getTarjetaCredito() {
+        return tarjetaCredito;
+    }
+    public void setTarjetaCredito(int tarjetaCredito) {
+        this.tarjetaCredito = tarjetaCredito;
+    }
+    public int getPuntosLicencia() {
+        return puntosLicencia;
+    }
+    public void setPuntosLicencia(int puntosLicencia) {
+        this.puntosLicencia = puntosLicencia;
+    }
+    public TipoCliente getTipoC() {
+        return tipoC;
+    }
+    public void setTipoC(TipoCliente tipoC) {
+        this.tipoC = tipoC;
+    }
     
 }
