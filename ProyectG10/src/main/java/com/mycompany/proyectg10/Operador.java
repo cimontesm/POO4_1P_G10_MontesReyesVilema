82cb0aae6244f68bcaf2a0ffb5060fc6f2beabe7
cmontes <cmontes@190.154.170.250>
2023-07-07 19:32:22 -0500

    avanzando

--- ProyectG10/src/main/java/com/mycompany/proyectg10/Operador.java
+++ ProyectG10/src/main/java/com/mycompany/proyectg10/Operador.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.proyectg10;
 
+import java.util.Scanner;
+
 /**
  *
  * @author cmontes
@@ -16,4 +18,46 @@ public abstract class Operador extends Usuario{
         String[] columnas = "operadores.txt".split(",");
         double sueldo;
     }
+    
+    public void registrarPago(){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("----------------");
+        System.out.println("REGISTRAR PAGOS");
+        System.out.println("----------------");
+        
+        System.out.println("Que desea pagar?");
+        System.out.println("1. Multa");
+        System.out.println("2. Revisión técnica");
+        System.out.println("Elija una opcion: ");
+        int tipoP = sc.nextInt();
+        
+        if(tipoP == 1){
+            consultarMultas();
+        }
+        
+        System.out.println("Valor a pagar: ");
+
+        
+        System.out.println("Que modo de pago va a usar?");
+        System.out.println("1. Efectivo");
+        System.out.println("2. Tarjeta de crédito");
+        System.out.println("Elija una opcion:  ");
+        int modoPago = sc.nextInt();
+
+        
+        System.out.print("Ingrese el monto a pagar: ");
+        double monto = sc.nextDouble();
+        
+
+    }
+    
+    @Override
+    public void consultarMultas(){
+        
+    }
+    
+//    public registrarUsuarios(){
+//    }
 }
