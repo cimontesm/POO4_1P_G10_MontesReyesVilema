58a7dbb147adadee391a45181059292100f0a90e
kiretigr <kiretigr@espol.edu.ec>
2023-07-08 15:32:27 -0500

    Agregar la implementación de la clase Multa.java

--- ProyectG10/src/main/java/com/mycompany/proyectg10/Multa.java
+++ ProyectG10/src/main/java/com/mycompany/proyectg10/Multa.java
@@ -9,5 +9,67 @@ package com.mycompany.proyectg10;
  * @author danie
  */
 public class Multa {
-    
+    private int puntos;
+    private Cliente cliente;
+    private String infraccion;
+    private double valorAPagar;
+    private String fechaInfraccion;
+    private String fechaNotificacion;
+
+    public Multa(int puntos, Cliente cliente, String infraccion, double valorAPagar, String fechaInfraccion, String fechaNotificacion) {
+        this.puntos = puntos;
+        this.cliente = cliente;
+        this.infraccion = infraccion;
+        this.valorAPagar = valorAPagar;
+        this.fechaInfraccion = fechaInfraccion;
+        this.fechaNotificacion = fechaNotificacion;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getInfraccion() {
+        return infraccion;
+    }
+
+    public void setInfraccion(String infraccion) {
+        this.infraccion = infraccion;
+    }
+
+    public double getValorAPagar() {
+        return valorAPagar;
+    }
+
+    public void setValorAPagar(double valorAPagar) {
+        this.valorAPagar = valorAPagar;
+    }
+
+    public String getFechaInfraccion() {
+        return fechaInfraccion;
+    }
+
+    public void setFechaInfraccion(String fechaInfraccion) {
+        this.fechaInfraccion = fechaInfraccion;
+    }
+
+    public String getFechaNotificacion() {
+        return fechaNotificacion;
+    }
+
+    public void setFechaNotificacion(String fechaNotificacion) {
+        this.fechaNotificacion = fechaNotificacion;
+    }
 }
