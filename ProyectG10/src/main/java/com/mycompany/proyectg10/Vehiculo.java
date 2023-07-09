7c361bef2627009873504df8e9e4e00ec98e6e1e
cmontes <cmontes@190.154.170.250>
2023-07-08 21:11:10 -0500

    clase vehiculo

--- ProyectG10/src/main/java/com/mycompany/proyectg10/Vehiculo.java
+++ ProyectG10/src/main/java/com/mycompany/proyectg10/Vehiculo.java
@@ -9,21 +9,69 @@ package com.mycompany.proyectg10;
  * @author cmontes
  */
 public class Vehiculo {
-//    String numPlaca;
-//    int cedula;
-//    String marca;
-//    int anio;
-//    String chasis;
-//    String color;
-    
-    
-    public void escribirArchivo(){
-        ManejoArchivos.LeeFichero("vehiculos.txt");
-        String[] columnas = "vehiculos.txt".split(",");
-        int cedula = "vehiculos.txt".indexOf(0);//en el archivo la primera columna eran cedulas no matriculas
-        
+
+    private String numPlaca;
+    private int cedula;
+    private String marca;
+    private int anio;
+    private String chasis;
+    private String color;
+
+    public Vehiculo(String numPlaca, int cedula, String marca, int anio, String chasis, String color) {
+        this.numPlaca = numPlaca;
+        this.cedula = cedula;
+        this.marca = marca;
+        this.anio = anio;
+        this.chasis = chasis;
+        this.color = color;
     }
-    
-    
-    
+
+    public String getNumPlaca() {
+        return numPlaca;
+    }
+
+    public void setNumPlaca(String numPlaca) {
+        this.numPlaca = numPlaca;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getChasis() {
+        return chasis;
+    }
+
+    public void setChasis(String chasis) {
+        this.chasis = chasis;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 }
